8cce986a8c6229aaae4dfab9702d19077577c320
qirele <kirilczikzik@gmail.com>
2022-03-20 14:54:14 +0100

    Added files

---
+++ CalculateTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+
+namespace scienceCalcCLI
+{
+    class CalculateTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(4, 4, 16)]
+        [TestCase(3, 1, 3)]
+        [TestCase(5, 2, 10)]
+        public void calculate_Multiplication_Test(int a, int b, double expectedResult)
+        {
+            var actual = Program.calculate($"calc {a} * {b}");
+
+            Assert.That(actual == expectedResult);
+
+
+        }
+
+
+        [TestCase(4, 4, 8)]
+        [TestCase(3, 1, 4)]
+        [TestCase(5, 2, 7)]
+        public void calculate_Addition_Test(int a, int b, double expectedResult)
+        {
+            var actual = Program.calculate($"calc {a} + {b}");
+
+            Assert.That(actual == expectedResult);
+
+        }
+
+        [TestCase(4, 4, 1)]
+        [TestCase(3, 6, 0.5)]
+        [TestCase(5, 2, 2.5)]
+        public void calculate_Division_Test(int a, int b, double expectedResult)
+        {
+            var actual = Program.calculate($"calc {a} / {b}");
+
+            Assert.That(actual == expectedResult);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using org.mariuszgromada.math.mxparser;
+
+namespace scienceCalcCLI
+{
+    public class Program
+    {
+
+        static List<Argument> argsList = new List<Argument>();
+
+        static void Main(string[] args)
+        {
+
+            var line = "";
+
+            do
+            {
+                line = Console.ReadLine().ToLower();
+                processInput(line);
+
+            } while (line != "quit");
+
+        }
+
+        static private void processInput(string line)
+        {
+            if (line.StartsWith("arg "))
+            {
+                addArgument(line);
+            }
+            else if (line.StartsWith("calc "))
+            {
+                Console.WriteLine(calculate(line));
+            }
+            else if (line == "help")
+            {
+                displayHelp();
+            }
+            else
+            {
+                Console.WriteLine(line == "quit" ? "" : "Don't know this kind of command");
+            }
+        }
+
+        static private void addArgument(string line)
+        {
+            var cells = line.Replace("arg ", string.Empty).Split(" ");
+            string name = cells[0];
+            var value = "";
+            for (int i = 1; i < cells.Length; i++)
+            {
+                value += cells[i];
+            }
+            Argument arg1 = new Argument($"{name} = {value}");
+            argsList.Add(arg1);
+        }
+
+
+        static public double calculate(string line)
+        {
+            var expString = line.Replace("calc ", string.Empty).Trim();
+            Expression e = new Expression(expString);
+            e.addArguments(argsList.ToArray());
+            return e.calculate();
+        }
+
+        static private void displayHelp()
+        {
+            Console.WriteLine("===Declaring arguments===");
+            Console.WriteLine("arg variableName value");
+            Console.WriteLine("arg x 25");
+            Console.WriteLine("arg y 15\n");
+            Console.WriteLine("===Evaluating expressions===");
+            Console.WriteLine("calc expression");
+            Console.WriteLine("calc (x + y) / 2");
+        }
+
+    }
+}
